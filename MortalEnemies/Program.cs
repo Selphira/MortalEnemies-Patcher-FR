b0a65cd432e380387809506beb0d734bac0d17b2
Selphira <80216629+Selphira@users.noreply.github.com>
2021-08-29 17:23:22 +0200

    Update Program.cs

--- MortalEnemies/Program.cs
+++ MortalEnemies/Program.cs
@@ -102,8 +102,8 @@ namespace MortalEnemies
                         string i18nRaceName = "";
                         if (patchedRace.Name != null 
                             
-                            && !patchedRace.Name.TryLookup(Language.French, out i18nRaceName)) {
-                            i18nRaceName = patchedRace.Name.String;
+                            && !patchedRace?.Name?.TryLookup(Language.French, out i18nRaceName)) {
+                            i18nRaceName = patchedRace?.Name.String;
                         }
                         patchedRace.Name = i18nRaceName;
                 
