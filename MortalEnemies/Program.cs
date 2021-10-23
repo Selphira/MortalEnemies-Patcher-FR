9fe74e174b61414d19c392ebe9c47c5bb9df9c80
Selphira <80216629+Selphira@users.noreply.github.com>
2021-10-23 23:05:19 +0200

    Update Program.cs

--- MortalEnemies/Program.cs
+++ MortalEnemies/Program.cs
@@ -103,7 +103,7 @@ namespace MortalEnemies
 
                 var patchedRace = state.PatchMod.Races.GetOrAddAsOverride(race);
                 
-                if (patchedRace.Name.TryLookup(Language.French, out string i18nRaceName)) {
+                if (patchedRace.Name != null && patchedRace.Name.TryLookup(Language.French, out string i18nRaceName)) {
                     patchedRace.Name = i18nRaceName;
                 }
                 
